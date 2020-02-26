40eeb132e64663c181ca1e67157d348f936812f2
Roman <rb@buechler.pro>
2020-02-26 10:14:45 +0100

    [TASK] Code fixes php files

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file is part of the package buepro/timelog.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 defined('TYPO3_MODE') || die();
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
