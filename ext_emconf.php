59ad7cce0b2af8ca055aaec0e4c706749dbf4384
Roman Büchler <rb@buechler.pro>
2022-11-15 20:29:08 +0100

    [TASK] Drop TYPO3 v10 support

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.7.3-dev',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.20-11.5.99',
+            'typo3' => '11.5.0-11.5.99',
             'auxlibs' => '1.4.0-1.99.99',
             'pvh' => '1.1.0-1.99.99',
         ],
